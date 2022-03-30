52fef0e50dce287ab7e0d86f8b0374803c29eef5
Kenzo Umezawa <kenzoumezawa@gmail.com>
2022-03-29 21:39:42 -0300

    Funcoes de manipulacoes de dados adicionada

---
+++ Aula-06/application/heroes_service.js
@@ -0,0 +1,97 @@
+const validate = require('validate.js');
+
+const Utils = require('../utils/utils');
+const Constants = require('../utils/constants');
+const HeroesRepository = require('../port/heroes_repository');
+const Constraints = require('../utils/heroes_validation');
+
+const Heroes = {
+    async create(data) {
+        try {
+            const validation = validate.validate(data, Constraints.create);
+            if (validation) {
+                const response = Constants.ErrorValidation;
+                response.message = validation;
+                return response;
+            }
+
+            data.id = Utils.generateUuid();
+
+            const response = await HeroesRepository.create(data);
+
+            if (response.code === 11000) {
+                const result = Constants.ErrorDuplicate;
+                return result;
+            }
+            return response;
+        } catch (error) {
+            return error;
+        }
+    },
+
+    async update(data) {
+        try {
+            const validation = validate.validate(data, Constraints.update);
+            if (validation) {
+                const response = Constants.ErrorValidation;
+                response.message = validation;
+                return response;
+            }
+
+            const response = await HeroesRepository.update(data);
+
+            if (response === []) {
+                const result = Constants.ErrorNotFound;
+                return result;
+            }
+            return response;
+        } catch (error) {
+            return error;
+        }
+    },
+
+    async delete(data) {
+        try {
+            const validation = validate.validate(data, Constraints.deleteBy);
+            if (validation) {
+                const response = Constants.ErrorValidation;
+                response.message = validation;
+                return response;
+            }
+
+            const response = await HeroesRepository.delete(data);
+
+            return response;
+        } catch (error) {
+            return error;
+        }
+    },
+
+    async listByName(data) {
+        try {
+            const validation = validate.validate(data, Constraints.get);
+            if (validation) {
+                const response = Constants.ErrorValidation;
+                response.message = validation;
+                return response;
+            }
+
+            const response = await HeroesRepository.getByName(data);
+
+            return response;
+        } catch (error) {
+            return error;
+        }
+    },
+
+    async list() {
+        try {
+            const response = await HeroesRepository.list();
+
+            return response;
+        } catch (error) {
+            return error;
+        }
+    },
+};
+module.exports = Heroes;
